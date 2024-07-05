5eb5577dbfebda5479f65e712b3eea6c80be0f0b
hyunseoko <kohyun0223@skuniv.ac.kr>
2024-07-05 22:30:12 +0900

    firebase 연동 (초기화)

---
+++ src/main/java/hanium/mathstudy/user/config/FirestoreConfig.java
@@ -0,0 +1,33 @@
+package hanium.mathstudy.user.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirestoreConfig {
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public Firestore firestore() {
+        return FirestoreClient.getFirestore();
+    }
+
+}
